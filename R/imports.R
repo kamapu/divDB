287280be135b1fa2ec4458626224e74ea6395a58
Miguel Alvarez <kamapu78@gmail.com>
2022-11-19 22:16:41 +0100

    Issues with 'compare_df()' solved

--- R/imports.R
+++ R/imports.R
@@ -1,4 +1,6 @@
-#' @importFrom DBI dbConnect dbDisconnect
+#' @importFrom biblio compare_df
+#' @importFrom DBI dbConnect dbDisconnect dbExistsTable dbReadTable
+#' @importFrom methods setMethod
 #' @importFrom RPostgres Postgres
 #' @importFrom tcltk tclVar tclvalue tkbind tkbutton tkdestroy tkentry tkfocus
 #'     tkgrid tkgrid.configure tklabel tktoplevel tkwait.window tkwm.title
