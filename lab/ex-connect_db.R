5a0816535a136ae540d2b5419d10a48c0aeafa12
Miguel Alvarez <kamapu78@gmail.com>
2024-01-02 09:42:57 +0100

    New examples for connect_db()

--- lab/ex-connect_db.R
+++ lab/ex-connect_db.R
@@ -10,3 +10,8 @@ conn <- connect_db(dbname = "test-db", user = "miguel")
 
 # Credentials not prompted (and error retrieved)
 conn <- connect_db(dbname = "test-db", user = "miguel2", password = "pw")
+
+# The same for package Rpostgres
+conn <- connect_db(dbname = "test-db", user = "miguel", pkg = "RPostgres")
+conn <- connect_db(dbname = "test-db", user = "miguel2", password = "pw",
+    pkg = "RPostgres")
