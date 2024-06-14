3de4bb9bcd04e444085f1531199d1a064a4f6120
Miguel Alvarez <kamapu78@gmail.com>
2024-06-14 12:19:10 +0200

    Ready for release. (#13)

--- lab/ex-update_rows.R
+++ lab/ex-update_rows.R
@@ -36,15 +36,20 @@ insert_rows(conn, subset(iris_df, species == "setosa"),
 
 # Retrieve the data from database
 iris_db <- dbGetQuery(conn, "select * from data_frames.iris")
+iris_db
 
 # Select and modify entries
-y <- iris_db[sample(1:nrow(iris_db), 5), c("id", "sepal_length", "petal_length")]
-y$sepal_length <- 100
-y$petal_length <- 100
+iris_mod <- iris_db[sample(1:nrow(iris_db), 5),
+    c("id", "sepal_length", "petal_length")]
+iris_mod$sepal_length <- 100
+iris_mod$petal_length <- 100
 
+# update database
+query <- update_rows(conn, iris_mod, c("data_frames", "iris"),
+    key = "id", eval = FALSE)
+query
 
+update_rows(conn, iris_mod, c("data_frames", "iris"), key = "id")
 
-x <- conn
-key = "id"
-name = c("data_frames", "iris")
-
+# cross-check
+dbGetQuery(conn, "select * from data_frames.iris")
